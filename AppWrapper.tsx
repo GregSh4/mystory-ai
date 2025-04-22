efbce06e0cb23d9f69e25e1d50b6c2b58d4ccd8f
GregSh4 <112956551+GregSh4@users.noreply.github.com>
2025-04-22 22:32:26 +0300

    Update AppWrapper.tsx

--- AppWrapper.tsx
+++ AppWrapper.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import PWAInstallPrompt from "./PWAInstallPrompt";
 
 // App wrapper component to include PWA functionality
 export default function AppWrapper({ children }) {
-  const [isClient, setIsClient] = React.useState(false);
-  
-  React.useEffect(() => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
     // This will only run on the client side
     setIsClient(true);
-    
+
     // Register service worker
     if ('serviceWorker' in navigator && typeof window !== 'undefined') {
-      window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(function (registration) {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
+        }, function (err) {
           console.log('ServiceWorker registration failed: ', err);
         });
       });
     }
   }, []);
-  
+
   return (
     <>
       {children}
